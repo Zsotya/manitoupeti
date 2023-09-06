f89e42d7cab16278938f2694abdc18301cc423e5
Zsotya <mocsok2000@gmail.com>
2023-09-06 13:35:05 +0200

    Created HomeWhoAreWe.vue component for Home page, added formatting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
     <div><HomeParagraphLeft /></div>
     <div><HomeParagraphRight /></div>
     <div><HomeParagraphLeft /></div>
+    <div><HomeWhoAreWe /></div>
   </div>
 </template>
 
@@ -13,6 +14,7 @@ import HomeLanding from "@/components/HomeLanding.vue";
 import HomeParagraphIntro from "@/components/HomeParagraphIntro.vue";
 import HomeParagraphLeft from "@/components/HomeParagraphLeft.vue";
 import HomeParagraphRight from "@/components/HomeParagraphRight.vue";
+import HomeWhoAreWe from "@/components/HomeWhoAreWe.vue";
 </script>
 
 <style scoped></style>
