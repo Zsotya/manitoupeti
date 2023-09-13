63de26d47d92ac487080df9155fa5028e8f21612
Zsotya <mocsok2000@gmail.com>
2023-09-13 18:17:35 +0200

    Updated CardComponent visuals

--- src/components/CareerComponents/CardComponent.vue
+++ src/components/CareerComponents/CardComponent.vue
@@ -3,6 +3,8 @@
     <div class="card-content">
       <div class="card-text-part">
         <div class="card-title">{{ job.jobName }}</div>
+
+        <div class="job-time">{{ job.jobTime }}</div>
         <div class="card-text-content">
           {{ job.jobDescription }}
         </div>
@@ -25,37 +27,61 @@ const { job } = defineProps(["job"]);
   border: 1px solid transparent;
   width: 410px;
   height: 460px;
-  background-color: white;
   margin-bottom: 20px;
+  border: 1px solid rgba(0, 0, 0, 0.25);
+  background-color: #f0f0f0;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background: linear-gradient(155deg, #f0f0f0, #ccc, #e6e6e6);
+  transition: 0.15s;
+  border-radius: 10px;
+}
+
+.career-card:hover {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  transform: scale(1.02);
+  transition: box-shadow 0.15s ease, transform 0.3s ease;
 }
 
 .card-title {
   font-size: 24px;
-  margin-bottom: 18px;
-  margin-top: 80px;
+  margin-bottom: 4px;
+  margin-top: 60px;
+  background: linear-gradient(45deg, #222, #444);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+}
+
+.job-time {
+  margin-bottom: 24px;
+  font-size: 14px;
 }
 
 .card-text-content {
-  font-size: 16px;
+  font-size: 18px;
   width: 30ch;
   text-align: left;
   margin-bottom: 16px;
   word-wrap: break-word;
+  text-align: justify;
 }
 
 .apply-button {
   position: relative;
-  background-color: transparent;
+  background-color: rgba(242, 243, 235, 0.513);
   color: black;
-  padding: 24px 74px;
+  padding: 24px 80px;
   cursor: pointer;
   border: 1px solid red;
   bottom: 50px;
-  transition: 0.8s ease;
+  transition: 0.4s;
+  border-radius: 5px;
+  font-weight: 600;
 }
 
 .apply-button:hover {
-  border: 1px solid black;
-  transition: 0.8s ease;
+  border: 1px solid rgb(53, 53, 53);
+  transition: 0.4s;
+  background-color: rgb(236, 171, 171);
 }
 </style>
--- src/views/Career.vue
+++ src/views/Career.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Logo />
+    <!-- <Logo /> -->
     <NavBar />
     <div class="career-cards">
       <CareerComp :jobs="jobs" />
@@ -21,48 +21,56 @@ const jobs = [
     jobName: "Emelőgépkezelő",
     jobDescription:
       "Szeretnénk felvenni minél több emelőgépkezelőt, mindenképpen elvárás az OKJ képzés megléte. Részletek a 'Jelentkezz most!' gombra kattintás után elérhetőek.",
+    jobTime: "Teljes munkaidő",
   },
   {
     id: 2,
     jobName: "Földmunkagépkezelő",
     jobDescription:
       "Szeretnénk felvenni minél több földmunkagépkezelőt, mindenképpen elvárás az OKJ képzés megléte. Részletek a 'Jelentkezz most!' gombra kattintás után elérhetőek.",
+    jobTime: "Teljes munkaidő",
   },
   {
     id: 3,
     jobName: "Fuvarszervező",
     jobDescription:
       "A fontos logisztikai feladatok elvégzésére szeretnénk új logisztikust/fuvarszervező munkatársat a köreinkbe. Amennyiben rendelkezik logisztikai végzettséggel és érdekelné a munkakör, kattintson a 'Jelentkezz most!' gombra.",
+    jobTime: "Teljes munkaidő",
   },
   {
     id: 4,
     jobName: "Könyvelő",
     jobDescription:
       "Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Kattints a 'Jelentkezz most!' gombra a részletekért.",
+    jobTime: "Részmunkaidő",
   },
   {
     id: 5,
     jobName: "Vevői kapcsolattartó",
     jobDescription:
       "Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Kattints a 'Jelentkezz most!' gombra a részletekért.",
+    jobTime: "Teljes munkaidő",
   },
   {
     id: 6,
     jobName: "Gépszerelő",
     jobDescription:
       "Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Kattints a 'Jelentkezz most!' gombra a részletekért.",
+    jobTime: "Részmunkaidő",
   },
   {
     id: 7,
     jobName: "Jogász",
     jobDescription:
-      "Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Kattints a 'Jelentkezz most!' gombra a részletekért.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+      "Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Kattints a 'Jelentkezz most!' gombra a részletekért.",
+    jobTime: "Részmunkaidő",
   },
   {
     id: 8,
     jobName: "Katona",
     jobDescription:
-      "Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Kattints a 'Jelentkezz most!' gombra a részletekért.aaaaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa ",
+      "Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Lorem ipsum aszkala baszkala csasztala bum. Prifferium tolerium mandatoru askelu kaotorikum. Kattints a 'Jelentkezz most!' gombra a részletekért.aaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaa ",
+    jobTime: "Alkalmi munka",
   },
 ];
 </script>
