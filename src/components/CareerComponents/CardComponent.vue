fd97279e1145d70b5074c698c9fc3688f9554448
Zsotya <mocsok2000@gmail.com>
2023-09-13 19:18:30 +0200

    Added clock svg and visibility modifications for CardComponent.vue

--- src/components/CareerComponents/CardComponent.vue
+++ src/components/CareerComponents/CardComponent.vue
@@ -3,7 +3,18 @@
     <div class="card-content">
       <div class="card-text-part">
         <div class="card-title">{{ job.jobName }}</div>
-
+        <div class="job-time-icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 512 512"
+          >
+            <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path
+              d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"
+            />
+          </svg>
+        </div>
         <div class="job-time">{{ job.jobTime }}</div>
         <div class="card-text-content">
           {{ job.jobDescription }}
@@ -31,7 +42,7 @@ const { job } = defineProps(["job"]);
   border: 1px solid rgba(0, 0, 0, 0.25);
   background-color: #f0f0f0;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  background: linear-gradient(155deg, #f0f0f0, #ccc, #e6e6e6);
+  background: linear-gradient(155deg, #f3e0c8, #dbc3a4, #c5a57e);
   transition: 0.15s;
   border-radius: 10px;
 }
@@ -49,12 +60,21 @@ const { job } = defineProps(["job"]);
   background: linear-gradient(45deg, #222, #444);
   -webkit-background-clip: text;
   background-clip: text;
-  color: transparent;
+  color: #555;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+.job-time-icon {
+  display: inline-block;
+  align-items: center;
+  vertical-align: middle;
+  margin-right: 6px;
 }
 
 .job-time {
+  display: inline-block;
   margin-bottom: 24px;
-  font-size: 14px;
+  font-size: 16px;
 }
 
 .card-text-content {
@@ -68,20 +88,22 @@ const { job } = defineProps(["job"]);
 
 .apply-button {
   position: relative;
-  background-color: rgba(242, 243, 235, 0.513);
-  color: black;
+  background: linear-gradient(135deg, #e5e5e5, #dcdcdc);
+  color: #333;
   padding: 24px 80px;
   cursor: pointer;
-  border: 1px solid red;
-  bottom: 50px;
-  transition: 0.4s;
+  border: none;
   border-radius: 5px;
+  bottom: 50px;
   font-weight: 600;
+  font-size: 15px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: background 0.4s ease, transform 0.2s ease;
 }
 
 .apply-button:hover {
-  border: 1px solid rgb(53, 53, 53);
-  transition: 0.4s;
-  background-color: rgb(236, 171, 171);
+  background: linear-gradient(135deg, #ccc, #c7c6c6);
+  transform: scale(1.05);
+  transition: background 0.4s ease, transform 0.2s ease;
 }
 </style>
