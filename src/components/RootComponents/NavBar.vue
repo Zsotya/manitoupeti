2a0950dee5414a67afef896fba813361b7f561d0
Zsotya <mocsok2000@gmail.com>
2023-09-18 18:46:51 +0200

    Added visuals to the language switch button

--- src/components/RootComponents/NavBar.vue
+++ src/components/RootComponents/NavBar.vue
@@ -9,13 +9,22 @@
       >
         <li>{{ $t(route.label) }}</li>
       </router-link>
-      <li>
-        <button
-          @click="$i18n.locale = $i18n.locale === 'hu' ? 'en' : 'hu'"
-          class="language-switch"
-        >
-          {{ $t("languageSwitch") }}
-        </button>
+      <li class="lang-switch">
+        <div class="onoffswitch">
+          <input
+            type="checkbox"
+            name="onoffswitch"
+            class="onoffswitch-checkbox"
+            id="myonoffswitch"
+            tabindex="0"
+            checked
+            @click="$i18n.locale = $i18n.locale === 'hu' ? 'en' : 'hu'"
+          />
+          <label class="onoffswitch-label" for="myonoffswitch">
+            <span class="onoffswitch-inner"></span>
+            <span class="onoffswitch-switch"></span>
+          </label>
+        </div>
       </li>
     </ul>
   </nav>
@@ -83,6 +92,85 @@ a {
   padding: 0px 20px;
   background-color: rgba(0, 0, 0, 0.538);
   transition: 0.3s ease-in-out;
+}
+
+.lang-switch {
+  text-decoration: none;
+  color: white;
+  padding: 10px 10px 0px 10px;
+  background-color: rgba(0, 0, 0, 0.538);
+  transition: 0.3s ease-in-out;
+  margin: 0px;
+}
+
+.onoffswitch {
+  position: relative;
+  width: 90px;
+}
+.onoffswitch-checkbox {
+  position: absolute;
+  opacity: 0;
+  pointer-events: none;
+}
+.onoffswitch-label {
+  display: block;
+  overflow: hidden;
+  cursor: pointer;
+  border: 2px solid #999999;
+  border-radius: 20px;
+}
+.onoffswitch-inner {
+  display: block;
+  width: 200%;
+  margin-left: -100%;
+  transition: margin 0.3s ease-in 0s;
+}
+.onoffswitch-inner:before,
+.onoffswitch-inner:after {
+  display: block;
+  float: left;
+  width: 50%;
+  height: 30px;
+  padding: 0;
+  line-height: 30px;
+  font-size: 14px;
+  color: white;
+  font-family: Trebuchet, Arial, sans-serif;
+  font-weight: bold;
+  box-sizing: border-box;
+}
+.onoffswitch-inner:before {
+  content: "HUN";
+  padding-left: 10px;
+  text-align: left;
+  background-color: #f0f0f0;
+  color: #000000;
+}
+.onoffswitch-inner:after {
+  content: "ENG";
+  padding-right: 10px;
+  background-color: #eeeeee;
+  color: #000000;
+  text-align: right;
+}
+.onoffswitch-switch {
+  display: block;
+  width: 20px;
+  margin: 5px;
+  background: #3498db;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 56px;
+  border: 2px solid #999999;
+  border-radius: 20px;
+  transition: all 0.3s ease-in 0s;
+}
+.onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-inner {
+  margin-left: 0;
+}
+.onoffswitch-checkbox:checked + .onoffswitch-label .onoffswitch-switch {
+  right: 0px;
 }
 
 /* Tablet nézet */
