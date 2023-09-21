4d57e432939f5074a9c1e375421f8be9717c6058
Zsotya <mocsok2000@gmail.com>
2023-09-21 14:15:16 +0200

    Added responsiveness for HomeParagraph.vue

--- src/components/HomeComponents/HomeParagraph.vue
+++ src/components/HomeComponents/HomeParagraph.vue
@@ -20,57 +20,102 @@ const isEven = paragraph.id % 2 === 0;
 
 <style scoped>
 .paragraph-section {
-  background-color: #e8e6e6;
-  padding: 80px;
-  padding-bottom: 0px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  background-color: #e8e6e6;
+  padding: 0rem 7.2vw 4rem 7.2vw;
 }
 
 .text-side {
-  flex: 1.6;
-  padding: 0px 75px;
   display: flex;
   flex-direction: column;
-  justify-content: left;
   align-self: center;
-}
-
-.image-side {
-  padding: 0px 75px;
+  flex-wrap: wrap;
 }
 
 .text-side h1 {
-  font-size: 30px;
   display: flex;
-  width: 30ch;
+  font-size: 1.875rem;
+  max-width: 30ch;
+  flex-wrap: wrap;
 }
 
 .text-side p {
-  font-size: 16px;
+  font-size: 1.1rem;
   line-height: 1.5;
-  width: 50ch;
-  padding: 16px 0px;
+  max-width: 60ch;
+  width: 100%;
+  padding: 0.625rem 0;
 }
 
 .image-side img {
-  width: 843px;
-  height: 896px;
+  width: 35rem;
+  height: 37.5rem;
   object-fit: cover;
-  max-width: 100%;
 }
 
 .reverse-order {
   flex-direction: row-reverse;
 }
 
-.reverse-order .text-side {
-  flex: 2;
-  padding: 0px 0px 0px 75px;
+.reverse-order .image-side img {
+  margin-right: 3rem;
 }
 
-.reverse-order .image-side {
-  flex: 3;
-  padding-left: 40px;
+/* Laptop nézet */
+
+@media screen and (max-width: 1024px) {
+  .text-side h1 {
+    font-size: 1.5rem;
+  }
+
+  .text-side p {
+    font-size: 1rem;
+    max-width: 50ch;
+  }
+
+  .image-side img {
+    width: 23rem;
+    height: 25rem;
+  }
 }
+
+/* Tablet nézet, mobil nézet */
+
+@media screen and (max-width: 768px) {
+  .paragraph-section {
+    flex-direction: column;
+  }
+
+  .text-side {
+    text-align: center;
+    padding-bottom: 1.5rem;
+  }
+
+  .text-side h1 {
+    justify-content: center;
+    max-width: 60ch;
+  }
+
+  .text-side p {
+    max-width: 76ch;
+  }
+
+  .image-side {
+    margin: 0 1rem 0 1rem;
+  }
+
+  .image-side img {
+    max-height: 34rem;
+    max-width: 32rem;
+    height: 100%;
+    width: 100%;
+  }
+
+  .reverse-order .image-side img {
+    margin-right: 0;
+  }
+}
+/* Külön mobilnézet nem szükséges */
 </style>
