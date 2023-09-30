00765f20bc9adc8fb6c269ecf3329a1375c41628
Zsotya <mocsok2000@gmail.com>
2023-09-30 22:00:42 +0200

    Created connection between frontend and backend for HomeParagraphs part. Data is now retrieved from server.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:8080", // így kap engedélyt middlewaren keresztül http requestek fogadásához
+  })
+);
 
 const paragraphsRoutes = require("./routes/paragraphs");
-
 app.use(paragraphsRoutes);
 
 // Start the server
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -21,38 +21,57 @@ import HomeParagraph from "@/components/HomeComponents/HomeParagraph.vue";
 import HomeWhoAreWe from "@/components/HomeComponents/HomeWhoAreWe.vue";
 import HomeGallery from "@/components/HomeComponents/HomeGallery.vue";
 
-const paragraphs = [
-  {
-    id: "1",
-    title_hu: "Első a biztonság",
-    title_en: "Safety first",
-    content_hu:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. ",
-    content_en:
-      "LoremEN ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam.",
-    imageSrc: require("@/assets/42.jpeg"),
-  },
-  {
-    id: "2",
-    title_hu: "Fontos az elégedettség",
-    title_en: "Satisfaction is important",
-    content_hu:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam.",
-    content_en:
-      "LoremEN ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam.",
-    imageSrc: require("@/assets/42.jpeg"),
-  },
-  {
-    id: "3",
-    title_hu: "Professzionális gépek, professzionális gépkezelők",
-    title_en: "Professional machines, professional operators",
-    content_hu:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam.",
-    content_en:
-      "LoremEN ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam.",
-    imageSrc: require("@/assets/42.jpeg"),
-  },
-];
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+const paragraphs = ref([]);
+async function fetchData() {
+  try {
+    const response = await axios.get("http://localhost:3000/api/paragraphs");
+    paragraphs.value = response.data;
+    console.log(paragraphs);
+    console.log(paragraphs.value[0].id);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+onMounted(() => {
+  fetchData();
+});
+
+// const paragraphs = [
+//   {
+//     id: "1",
+//     title_hu: "Első a biztonság",
+//     title_en: "Safety first",
+//     content_hu:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. ",
+//     content_en:
+//       "LoremEN ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam.",
+//     imageSrc: require("@/assets/42.jpeg"),
+//   },
+//   {
+//     id: "2",
+//     title_hu: "Fontos az elégedettség",
+//     title_en: "Satisfaction is important",
+//     content_hu:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam.",
+//     content_en:
+//       "LoremEN ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam.",
+//     imageSrc: require("@/assets/42.jpeg"),
+//   },
+//   {
+//     id: "3",
+//     title_hu: "Professzionális gépek, professzionális gépkezelők",
+//     title_en: "Professional machines, professional operators",
+//     content_hu:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam.",
+//     content_en:
+//       "LoremEN ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore et adipisci sapiente tempora quisquam tempore iste. Cupiditate quo eos odit delectus quasi, numquam repellendus id facere totam natus ullam nam.",
+//     imageSrc: require("@/assets/42.jpeg"),
+//   },
+// ];
 </script>
 
 <style scoped></style>
