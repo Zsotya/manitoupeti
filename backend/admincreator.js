00c4486a72e7401dfaa5365074d043b0d2bc9a37
Zsotya <mocsok2000@gmail.com>
2023-10-03 20:25:58 +0200

    Using bcryptjs instead of bcrypt for admincreator.js

--- backend/admincreator.js
+++ backend/admincreator.js
@@ -1,5 +1,5 @@
 const readline = require("readline");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const mysql = require("mysql2/promise");
 
 const rl = readline.createInterface({
