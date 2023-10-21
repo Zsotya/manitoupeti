da082e3082ce346c1bd20d4dc31ddc3f1342bd71
Zsotya <mocsok2000@gmail.com>
2023-10-21 22:23:44 +0200

    Modified FullCalendar to display events with their respective colors and titles

--- frontend/src/components/MachineDetailComponents/FullCalendar.vue
+++ frontend/src/components/MachineDetailComponents/FullCalendar.vue
@@ -11,34 +11,21 @@
 import FullCalendar from "@fullcalendar/vue3";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import axios from "axios";
+import { ref, onMounted, watch } from "vue";
 
 // const testButton = () => {
-//   console.log("Pending:", pendingData);
-//   console.log("Accepted:", acceptedData);
-//   console.log("Paid:", paidData);
+//   console.log(calendarOptions.events);
 // };
 
-// Jelenlegi dátum kiszámolása
+// Jelenlegi dátum kiszámolása és formázása
 const startDate = new Date().toISOString().split("T")[0];
 
-// 5 hónap múlva levő dátum kiszámolása
+// 5 hónap múlva levő dátum kiszámolása és formázása
 const endDate = new Date(new Date().setMonth(new Date().getMonth() + 5))
   .toISOString()
   .split("T")[0];
 
-// FullCalendar konfigurálása
-const calendarOptions = {
-  plugins: [dayGridPlugin],
-  initialView: "dayGridMonth",
-  height: 850,
-  validRange: {
-    start: startDate,
-    end: endDate,
-  },
-  events: [],
-};
-
-// Adatok lekérdezése
+// GET request - Adatok lekérdezése
 const getBookingData = async (status) => {
   try {
     const response = await axios.get(
@@ -51,10 +38,82 @@ const getBookingData = async (status) => {
   }
 };
 
-// Rekordok tárolása
-const pendingData = getBookingData("pending");
-const acceptedData = getBookingData("accepted");
-const paidData = getBookingData("paid");
+// Események feltöltése adatokkal
+const events = ref([]);
+const createCalendarEvents = async () => {
+  const pendingData = await getBookingData("pending");
+  const paidData = await getBookingData("paid");
+  const acceptedData = await getBookingData("accepted");
+  // Dátumok formázása. Azért külön, mert end esetén +1 napot hozzá kell adni (FullCalendar működése miatt..)
+  function formatStart(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+  function formatEnd(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate() + 1).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+
+  // Adatok feldolgozása és FullCalendar-nak megfelelő formátummá konvertálása
+  // eventsData.end esetén +1 napot hozzáadunk, különben nem megfelelő date-ranget színez
+  const eventsData = [
+    ...pendingData.map((booking) => ({
+      title: "Pending",
+      start: formatStart(new Date(booking.start_date)),
+      end: formatEnd(new Date(booking.end_date)),
+      backgroundColor: "orange",
+      display: "background",
+    })),
+
+    ...acceptedData.map((booking) => ({
+      title: "Accepted",
+      start: formatStart(new Date(booking.start_date)),
+      end: formatEnd(new Date(booking.end_date)),
+      backgroundColor: "orange",
+      display: "background",
+    })),
+
+    ...paidData.map((booking) => ({
+      title: "Paid",
+      start: formatStart(new Date(booking.start_date)),
+      end: formatEnd(new Date(booking.end_date)),
+      backgroundColor: "red",
+      display: "background",
+    })),
+  ];
+  events.value = eventsData;
+};
+
+// FullCalendar konfigurálása
+
+const calendarOptions = {
+  plugins: [dayGridPlugin],
+  initialView: "dayGridMonth",
+  height: 850,
+  validRange: {
+    start: startDate,
+    end: endDate,
+  },
+  events: events,
+  firstDay: 1,
+  showNonCurrentDates: false,
+  fixedWeekCount: false,
+};
+
+// Kezdeti inicializáláskor a calendarOptions.events értéke undefined lenne, mivel hamarabb renderel, mint hogy a GET request megtörténne.
+// Éppen ezért watchert használunk, hogy "rákényszerítsük a FullCalendar-t", hogy re-rendereljen, ha változik az adat
+
+onMounted(() => {
+  createCalendarEvents();
+});
+
+watch(events, (newEvents) => {
+  calendarOptions.events = newEvents;
+});
 </script>
 
 <style scoped>
