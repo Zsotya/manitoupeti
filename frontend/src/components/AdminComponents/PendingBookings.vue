a63a1fdc0ad0ae2ecbe4cec9790a4a500cb78610
Zsotya <mocsok2000@gmail.com>
2023-11-02 21:14:01 +0100

    Created Admin Bookings page to display bookings for different statuses.Functionality and styling has not been added yet

---
+++ frontend/src/components/AdminComponents/PendingBookings.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="admin-pending-bookings">
+    <div class="title">
+      <h2>Függőben lévő foglalások</h2>
+    </div>
+    <div class="bookings-table">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nehézgép ID</th>
+            <th>Teljes név</th>
+            <th>E-mail</th>
+            <th>Telefonszám</th>
+            <th>Helyszín</th>
+            <th>Időintervallum</th>
+            <th>Ár</th>
+            <th>Létrehozás dátuma</th>
+            <th>Műveletek</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="booking in pendingBookings" :key="booking.id">
+            <td>{{ booking.id }}</td>
+            <td>{{ booking.machine_id }}</td>
+            <td>{{ `${booking.first_name} ${booking.last_name}` }}</td>
+            <td>{{ booking.email }}</td>
+            <td>{{ booking.phone_number }}</td>
+            <td>{{ booking.location }}</td>
+            <td>{{ formatDateRange(booking.start_date, booking.end_date) }}</td>
+            <td>{{ booking.price }}Ft</td>
+            <td>{{ booking.created_at }}</td>
+            <td class="actions-buttons">
+              <button
+                class="approve-button"
+                @click="approveBooking(booking.id)"
+              >
+                <i class="fas fa-check"></i>Jóváhagyás
+              </button>
+              <button class="reject-button" @click="rejectBooking(booking.id)">
+                <i class="fas fa-times"></i>Elutasítás
+              </button>
+              <button
+                class="modify-price-button"
+                @click="modifyPrice(booking.id)"
+              >
+                <i class="fas fa-edit"></i>Ár módosítása
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const pendingBookings = ref([]);
+
+async function fetchData() {
+  try {
+    const response = await axios.get(
+      "http://localhost:3000/api/bookings/pending"
+    );
+    pendingBookings.value = response.data;
+  } catch (error) {
+    console.error("Error fetching pending bookings:", error);
+  }
+}
+
+onMounted(() => {
+  fetchData();
+});
+
+// Időintervallum YYYY-MM-DD formátumra formázás (ebben az esetben az óra,perc irreleváns)
+const formatDateRange = (startDate, endDate) => {
+  const formattedStartDate = new Date(startDate).toISOString().split("T")[0];
+  const formattedEndDate = new Date(endDate).toISOString().split("T")[0];
+
+  return `${formattedStartDate} - ${formattedEndDate}`;
+};
+</script>
--- frontend/src/views/AdminViews/AdminBookings.vue
+++ frontend/src/views/AdminViews/AdminBookings.vue
@@ -1,3 +1,21 @@
-<template>Test</template>
+<template>
+  <PendingBookings />
+  <ApprovedBookings />
+  <ExpiredBookings />
+</template>
 
-<script setup></script>
+<script setup>
+import PendingBookings from "@/components/AdminComponents/PendingBookings.vue";
+import ApprovedBookings from "@/components/AdminComponents/ApprovedBookings.vue";
+import ExpiredBookings from "@/components/AdminComponents/ExpiredBookings.vue";
+import { onMounted } from "vue";
+import authService from "@/services/authService";
+
+onMounted(() => {
+  const token = localStorage.getItem("token");
+  if (authService.isTokenExpired(token)) {
+    authService.logoutUser();
+    $router.push("/admin");
+  }
+});
+</script>
