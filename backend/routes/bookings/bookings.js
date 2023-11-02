db49555bc26951cea46cbf89a5287cbaa395fab4
Zsotya <mocsok2000@gmail.com>
2023-11-02 17:49:22 +0100

    Added GET request endpoint for bookings.js

--- backend/routes/bookings/bookings.js
+++ backend/routes/bookings/bookings.js
@@ -2,6 +2,19 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db");
 
+// GET REQUEST KEZELÉSE - Bookingok lekérdezése
+
+router.get("/api/bookings", (req, res) => {
+  db.query("SELECT * FROM bookings", (err, results) => {
+    if (err) {
+      console.error("Error querying the database:", err);
+      res.status(500).json({ error: "Database error" });
+      return;
+    }
+    res.json(results);
+  });
+});
+
 // POST REQUEST KEZELÉSE - Új booking létrehozása
 
 router.post("/api/bookings", (req, res) => {
