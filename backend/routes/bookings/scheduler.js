eca3bfdbe158480cf9a08a864bb1d9fd13d08b67
Zsotya <mocsok2000@gmail.com>
2023-11-06 21:35:29 +0100

    Remodeled update function in scheduler.Added archive function to scheduler.js

--- backend/routes/bookings/scheduler.js
+++ backend/routes/bookings/scheduler.js
@@ -3,23 +3,67 @@ const cron = require("node-cron");
 
 function scheduleBookingExpirationCheck() {
   // Éjfélre időzítés
-  cron.schedule("0 0 * * *", async () => {
+  cron.schedule("00 00 * * *", async () => {
     try {
       // 3 nappal ezelőtti nap kiszámítása
       const threeDaysAgo = new Date();
       threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
 
-      // Approved státuszú bookingok listázása és Expired státuszra állítása, ha 3 napnál régebbi
-      const query = `
+      // Irreleváns (end_date régebbi a mai napnál) bookingok másolása az archive_bookings táblába
+      const archiveQuery = `
+        INSERT INTO archive_bookings (booking_id, machine_id, first_name, last_name, email, phone_number, location, start_date, end_date, status, price, created_at, comment)
+        SELECT id, machine_id, first_name, last_name, email, phone_number, location, start_date, end_date, status, price, created_at, comment
+        FROM bookings
+        WHERE end_date < NOW();
+      `;
+
+      // A másolt booking törlése a bookings táblából
+      const deleteQuery = `
+        DELETE FROM bookings
+        WHERE end_date < NOW();
+      `;
+
+      // Approved státuszú bookingok listázása és Expired státuszra állítása, ha x napnál régebbi
+      const updateQuery = `
           UPDATE bookings
           SET status = 'Expired'
           WHERE status = 'Approved' AND updated_at < ?;
         `;
 
-      const [results] = await db.execute(query, [threeDaysAgo]);
-      console.log(`Updated ${results.affectedRows} bookings to 'Expired'.`);
+      // Queryk lefuttatása
+      // archiveQuery
+      db.query(archiveQuery, (archiveError, archiveResults) => {
+        if (archiveError) {
+          console.error("Hiba archiválás közben:", archiveError);
+        } else {
+          console.log(
+            `Másolás sikeres. Másolt sorok száma: ${archiveResults.affectedRows}`
+          );
+          // deleteQuery
+          db.query(deleteQuery, (deleteError, deleteResults) => {
+            if (deleteError) {
+              console.error("Hiba törlés közben:", deleteError);
+            } else {
+              console.log(
+                `Törlés sikeres. Törölt sorok száma: ${deleteResults.affectedRows}`
+              );
+              console.log("Sikeres archiválás!");
+            }
+          });
+        }
+      });
+      // updateQuery
+      db.query(updateQuery, [threeDaysAgo], (updateError, updateResults) => {
+        if (updateError) {
+          console.error("Hiba a státusz módosításakor:", updateError);
+        } else {
+          console.log(
+            `Lejárt foglalások státusza sikeresen módosítva "Rejected" státuszra. Módosított sorok száma: ${updateResults.affectedRows}`
+          );
+        }
+      });
     } catch (error) {
-      console.error("Error updating bookings:", error);
+      console.error("Hiba:", error);
     }
   });
 }
