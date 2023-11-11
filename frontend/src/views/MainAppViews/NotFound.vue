499e8980e8094e4e2c3c4514021b7f1b9cb16e85
Zsotya <mocsok2000@gmail.com>
2023-11-12 00:39:03 +0100

    Created basic layout for 404 page

--- frontend/src/views/MainAppViews/NotFound.vue
+++ frontend/src/views/MainAppViews/NotFound.vue
@@ -1,5 +1,42 @@
 <template>
-  <p>Oldal nem található, navigálj máshova!</p>
+  <div class="container">
+    <div class="content">
+      <h1>404</h1>
+      <p>Hoppá! Az Ön által keresett oldal nem található.</p>
+      <p>
+        Kérjük, használja a navigációs menüt az oldalon történő navigáláshoz.
+      </p>
+      <router-link to="/">Navigálás a főoldalra</router-link>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #e8e6e6;
+}
+
+.content {
+  text-align: center;
+}
+
+h1 {
+  font-size: 72px;
+  color: red;
+}
+
+p {
+  font-size: 24px;
+  margin: 12px 0px;
+}
+
+a {
+  font-size: 24px;
+  text-decoration: none;
+  color: blue;
+}
+</style>
