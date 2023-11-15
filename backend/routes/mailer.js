f5e6eff9a44d354824d8e895efe0d431747851ab
Zsotya <mocsok2000@gmail.com>
2023-11-15 16:16:39 +0100

    Created CareerDetail.vue with email sending functionality,added API endpoint for this in mailer.js.Added responsive design and dark theme as well

--- backend/routes/mailer.js
+++ backend/routes/mailer.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const router = express.Router();
+const multer = require("multer");
 
 // Feladó adatok meghatározása
 const transporter = nodemailer.createTransport({
@@ -34,5 +35,80 @@ router.post("/api/contactus", (req, res) => {
     }
   });
 });
+
+// Állásra jelentkező emailek kezelése
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.post(
+  "/api/job-application",
+  upload.single("resume"),
+  async (req, res) => {
+    try {
+      const {
+        lastName,
+        firstName,
+        birthDate,
+        birthLocation,
+        email,
+        phoneNumber,
+        zipCode,
+        settlement,
+        district,
+        publicAreaName,
+        publicAreaType,
+        houseNumber,
+        building,
+        stairway,
+        floor,
+        door,
+        jobName,
+      } = req.body;
+      const resume = req.file.buffer;
+
+      // Form adatok előkészítése
+      const fullName = `${lastName} ${firstName}`;
+      const districtFormat = district ? `${district} kerület` : "";
+      const buildingFormat = building ? `${building} épület` : "";
+      const stairwayFormat = stairway ? `${stairway} lépcsőház` : "";
+      const floorFormat = floor ? `${floor} emelet` : "";
+      const doorFormat = door ? `${door} ajtó` : "";
+      const address = `${zipCode} ${settlement} ${districtFormat} ${publicAreaName} ${publicAreaType} ${houseNumber} ${buildingFormat} ${stairwayFormat} ${floorFormat} ${doorFormat}`;
+
+      // Fájlnév sztring
+      const currentDate = new Date()
+        .toISOString()
+        .slice(0, 10)
+        .replace(/-/g, "");
+      const fileName = `${fullName} ${currentDate}.pdf`;
+
+      // Email beállítása
+      const mailOptions = {
+        from: "manitoupetinoreply@gmail.com",
+        to: "manitoupetinoreply@gmail.com",
+        subject: `Új állásjelentkezés - ${jobName}`,
+        text: `A ManitouPeti weboldalon Önnek új állásjelentkezése érkezett a ${jobName} állásra. Az ehhez tartozó adatok a következők:\n\nNév: ${fullName}\nSzületési idő: ${birthDate}\nSzületési hely: ${birthLocation}\nEmail: ${email}\nTelefonszám: ${phoneNumber}\nLakhely: ${address}\nMegpályázott munkakör: ${jobName}\n\nA jelentkezéshez csatolt önéletrajzot az email csatolmányai között tekintheti meg.\n\nManitouPeti`,
+        attachments: [
+          {
+            filename: fileName,
+            content: resume,
+            encoding: "base64",
+          },
+        ],
+      };
+
+      // Email küldése
+      const info = await transporter.sendMail(mailOptions);
+      console.log("Email elküldve:", info.response);
+
+      res.json({ success: "Jelentkezési email sikeresen elküldve!" });
+    } catch (error) {
+      console.error("Hiba a jelentkezési email elküldésekor:", error);
+      res
+        .status(500)
+        .json({ error: "Hiba a jelentkezési email elküldésekor!" });
+    }
+  }
+);
 
 module.exports = router;
--- frontend/src/views/MainAppViews/CareerDetail.vue
+++ frontend/src/views/MainAppViews/CareerDetail.vue
@@ -1,11 +1,160 @@
-<template>{{ job.jobname_hu }} munka oldala</template>
+<template>
+  <div class="page-container" :class="{ 'dark-mode': darkMode }">
+    <form @submit.prevent="submitForm" class="styled-form">
+      <div class="form-title">Jelentkezés {{ job.jobname_hu }} állásra</div>
+      <!-- Név -->
+      <div class="name-part">
+        <div class="form-group">
+          <label for="lastName"
+            ><span class="required">*</span>Vezetéknév:</label
+          >
+          <input type="text" id="lastName" v-model="lastName" required />
+        </div>
+        <div class="form-group">
+          <label for="firstName"
+            ><span class="required">*</span>Keresztnév:</label
+          >
+          <input type="text" id="firstName" v-model="firstName" required />
+        </div>
+      </div>
+      <!-- Születési dátum -->
+      <div class="birth-part">
+        <div class="form-group">
+          <label for="birthDate"
+            ><span class="required">*</span>Születési dátum:</label
+          >
+          <input type="date" id="birthDate" v-model="birthDate" required />
+        </div>
+        <div class="form-group">
+          <label for="birthLocation"
+            ><span class="required">*</span>Születési hely:</label
+          >
+          <input
+            type="text"
+            id="birthLocation"
+            v-model="birthLocation"
+            required
+          />
+        </div>
+      </div>
+      <!-- Email -->
+      <div class="email-part">
+        <div class="form-group">
+          <label for="email"><span class="required">*</span>Email:</label>
+          <input type="email" id="email" v-model="email" required />
+        </div>
+      </div>
+      <!-- Telefonszám, irányítószám -->
+      <div class="phone-zip-part">
+        <div class="form-group">
+          <label for="phoneNumber"
+            ><span class="required">*</span>Telefonszám:</label
+          >
+          <input type="tel" id="phoneNumber" v-model="phoneNumber" required />
+        </div>
+        <div class="form-group">
+          <label for="zipCode"
+            ><span class="required">*</span>Irányítószám:</label
+          >
+          <input type="text" id="zipCode" v-model="zipCode" required />
+        </div>
+      </div>
+      <!-- Város, kerület -->
+      <div class="settlement-district-part">
+        <div class="form-group">
+          <label for="settlement"
+            ><span class="required">*</span>Település:</label
+          >
+          <input type="text" id="settlement" v-model="settlement" required />
+        </div>
+        <div class="form-group">
+          <label for="district">Kerület:</label>
+          <input type="text" id="district" v-model="district" />
+        </div>
+      </div>
+      <!-- Közterület típus, jelleg -->
+      <div class="public-area-part">
+        <div class="form-group">
+          <label for="publicAreaName"
+            ><span class="required">*</span>Közterület neve:</label
+          >
+          <input
+            type="text"
+            id="publicAreaName"
+            v-model="publicAreaName"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <label for="publicAreaType"
+            ><span class="required">*</span>Közterület jellege:</label
+          >
+          <input
+            type="text"
+            id="publicAreaType"
+            v-model="publicAreaType"
+            required
+          />
+        </div>
+      </div>
+      <!-- Ház adatok -->
+      <div class="house-part">
+        <div class="form-group">
+          <label for="houseNumber"
+            ><span class="required">*</span>Házszám:</label
+          >
+          <input type="text" id="houseNumber" v-model="houseNumber" required />
+        </div>
+        <div class="form-group">
+          <label for="building">Épület:</label>
+          <input type="text" id="building" v-model="building" />
+        </div>
+        <div class="form-group">
+          <label for="stairway">Lépcsőház:</label>
+          <input type="text" id="stairway" v-model="stairway" />
+        </div>
+        <div class="form-group">
+          <label for="floor">Emelet:</label>
+          <input type="text" id="floor" v-model="floor" />
+        </div>
+        <div class="form-group">
+          <label for="door">Ajtó:</label>
+          <input type="text" id="door" v-model="door" />
+        </div>
+      </div>
+      <!-- Önéletrajz -->
+      <div class="form-group">
+        <label for="resume"><span class="required">*</span>Önéletrajz:</label>
+        <input
+          type="file"
+          id="resume"
+          ref="resumeInput"
+          @change="handleFileChange"
+          required
+        />
+      </div>
+      <!-- Küldés -->
+      <div class="button-part">
+        <div class="submit-button">
+          <button type="submit">Küldés</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
 
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRoute } from "vue-router";
 import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 import axios from "axios";
 
+/* Dark mode */
+const store = useStore();
+const darkMode = computed(() => store.getters.isDarkMode);
+
+// Adatok fetchelése
 const route = useRoute();
 const router = useRouter();
 const job = ref([]);
@@ -37,4 +186,197 @@ async function fetchData() {
 onMounted(() => {
   fetchData();
 });
+
+// Form elemek inicializálása
+const lastName = ref("");
+const firstName = ref("");
+const birthDate = ref("");
+const birthLocation = ref("");
+const email = ref("");
+const phoneNumber = ref("");
+const zipCode = ref("");
+const settlement = ref("");
+const district = ref("");
+const publicAreaName = ref("");
+const publicAreaType = ref("");
+const houseNumber = ref("");
+const building = ref("");
+const stairway = ref("");
+const floor = ref("");
+const door = ref("");
+const resume = ref(null);
+
+// Önéletrajz fájl tárolása
+function handleFileChange(event) {
+  resume.value = event.target.files[0];
+}
+
+// Form elküldése
+async function submitForm() {
+  try {
+    const formData = new FormData();
+    formData.append("lastName", lastName.value);
+    formData.append("firstName", firstName.value);
+    formData.append("birthDate", birthDate.value);
+    formData.append("birthLocation", birthLocation.value);
+    formData.append("email", email.value);
+    formData.append("phoneNumber", phoneNumber.value);
+    formData.append("zipCode", zipCode.value);
+    formData.append("settlement", settlement.value);
+    formData.append("district", district.value);
+    formData.append("publicAreaName", publicAreaName.value);
+    formData.append("publicAreaType", publicAreaType.value);
+    formData.append("houseNumber", houseNumber.value);
+    formData.append("building", building.value);
+    formData.append("stairway", stairway.value);
+    formData.append("floor", floor.value);
+    formData.append("door", door.value);
+    formData.append("resume", resume.value);
+    formData.append("jobName", job.value.jobname_hu);
+    const response = await axios.post(
+      `http://localhost:3000/api/job-application`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    console.log("Jelentkezés sikeresen elküldve:", response.data);
+  } catch (error) {
+    console.error("Hiba a jelentkezés elküldésekor:", error);
+  }
+}
 </script>
+
+<style scoped>
+.page-container {
+  background-color: #e8e6e6;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  padding-top: 150px;
+  padding-bottom: 30px;
+  color: #222;
+  transition: background-color 0.5s, color 0.5s;
+}
+
+.styled-form {
+  max-width: 840px;
+  margin: 0 auto;
+  min-height: 70vh;
+  background-color: #f5f5f5;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  transition: background-color 0.5s, border 0.5s;
+}
+
+.form-title {
+  font-size: 1.5em;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+.name-part,
+.birth-part,
+.email-part,
+.phone-zip-part,
+.settlement-district-part,
+.public-area-part,
+.house-part {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+}
+
+.form-group {
+  flex: 1;
+  margin-right: 10px;
+}
+
+label {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+.required {
+  color: red;
+  margin-right: 4px;
+}
+
+input {
+  width: 100%;
+  padding: 8px;
+  box-sizing: border-box;
+  border: 1px solid #ced4da;
+  border-radius: 4px;
+  transition: background-color 0.5s, border 0.5s;
+}
+
+.button-part {
+  margin-top: 10px;
+  display: flex;
+}
+
+button {
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 1rem;
+  transition: background-color 0.3s;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+@media screen and (max-width: 496px) {
+  .name-part,
+  .birth-part,
+  .email-part,
+  .phone-zip-part,
+  .settlement-district-part,
+  .public-area-part,
+  .house-part {
+    flex-direction: column;
+    margin: 0px;
+  }
+
+  .form-group {
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
+  input {
+    width: 80%;
+  }
+}
+
+/* Dark mode */
+.page-container.dark-mode {
+  background-color: #1a1a1a;
+  color: #eeeeee;
+}
+
+.page-container.dark-mode input {
+  background-color: #212121;
+  border: 1px solid #ffffff;
+  color: white;
+}
+
+.page-container.dark-mode .styled-form {
+  background-color: #303030;
+  border: 1px solid #ddd;
+}
+
+.page-container.dark-mode button {
+  background-color: #0056b3;
+}
+
+.page-container.dark-mode button:hover {
+  background-color: #007bff;
+}
+</style>
