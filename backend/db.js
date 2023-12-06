964427a2e7e666b1739df8e589838e6c2b1406f2
Zsotya <mocsok2000@gmail.com>
2023-12-06 17:15:02 +0100

    Added dotenv library to the project,added environment variables users can use to create .env files

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,13 @@
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "manitoupetidb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // Connection tesztelés
