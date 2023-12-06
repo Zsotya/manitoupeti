5df518b686e185ff7556d4cfe45f6f7e8691571b
Zsotya <mocsok2000@gmail.com>
2023-12-06 20:32:23 +0100

    Added function to db.js to retry connection if the connection fails,added graceful exit instead of hard shutdown

--- backend/db.js
+++ backend/db.js
@@ -2,21 +2,42 @@ const mysql = require("mysql2");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const connection = createConnection();
+
+// Kapcsolat létrehozása funkció
+function createConnection() {
+  return mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+}
+
+// Kapcsolódás kezelése, újrapróbálkozási logika
+function handleConnection() {
+  // Kapcsolódási kísérlet
+  connection.connect((err) => {
+    if (err) {
+      console.error("Hiba az adatbázishoz kapcsolódáskor:", err);
+      // Kapcsolódás újrapróbálása időzítés
+      console.log("Újrapróbálkozás 5 másodpercen belül...");
+      setTimeout(handleConnection, 5000);
+    } else {
+      console.log("Sikeres kapcsolódás az adatbázishoz");
+    }
+  });
+}
+
+// Kezdeti csatlakozás
+handleConnection();
+
+// Könnyed bezárás azonnal shut helyett
+process.on("SIGINT", () => {
+  connection.end();
+  process.exit();
 });
 
-// Connection tesztelés
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err);
-    return;
-  }
-  console.log("Connected to the database");
-});
-
+// Exportálás
 module.exports = connection;
