83ba8753e5fcc166550e1cfe999f9e378fffafa6
Zsotya <mocsok2000@gmail.com>
2023-12-11 18:22:56 +0100

    Fixed delete error,made db connection universal,fixed comments and messages in admins.js

--- backend/routes/admins.js
+++ backend/routes/admins.js
@@ -2,78 +2,66 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 const bcrypt = require("bcryptjs");
-const mysql = require("mysql2/promise");
 
-// GET REQUEST KEZELÉSE - Adminok listázása (jelszó nélkül, főadminok kivételével)
-
+// Adminok listázása (jelszó nélkül, főadminok kivételével) - GET
 router.get("/api/admins", (req, res) => {
+  // Lekérdezés
   db.query(
     "SELECT id, username, full_name FROM admin_users WHERE is_main_admin = 0",
     (err, results) => {
+      // Hibakezelés
       if (err) {
-        console.error("Error querying the database:", err);
-        res.status(500).json({ error: "Database error" });
+        console.error("Hiba az adatbázis lekérdezése közben:", err);
+        res.status(500).json({ error: "Adatbázis hiba" });
         return;
       }
+      // Sikeres lekérdezés esetén az eredmény visszaküldése
       res.json(results);
     }
   );
 });
 
-// POST REQUEST KEZELÉSE - Új admin létrehozása
-
+// Új admin létrehozása - POST
 router.post("/api/admins", async (req, res) => {
   const { username, password, full_name } = req.body;
   // Jelszó hashelés
   const saltRounds = 13;
   try {
-    const connection = await mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "password",
-      database: "manitoupetidb",
-    });
-
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    const [result] = await connection.execute(
+    const result = await db.execute(
       "INSERT INTO admin_users (username, password, full_name) VALUES (?, ?, ?)",
       [username, hashedPassword, full_name]
     );
-
-    connection.end();
-
-    res.status(201).json({ message: "Admin user created successfully" });
+    // Sikeres létrehozás
+    res.status(201).json({ message: "Admin felhasználó sikeresen létrehozva" });
   } catch (error) {
-    console.error("Error creating admin user:", error);
-    res.status(500).json({ error: "Error creating admin user" });
+    // Hibakezelés
+    console.error("Hiba az admin felhasználó létrehozása közben:", error);
+    res.status(500).json({ error: "Hiba az admin létrehozásakor" });
   }
 });
 
-// DELETE REQUEST KEZELÉSE - Admin eltávolítása
+// Admin eltávolítása - DELETE
 
 router.delete("/api/admins/:id", async (req, res) => {
+  // Azonosító meghatározása
   const adminId = req.params.id;
-  // Extra biztonságért: Főadmin törlése ne legyen lehetséges
-  if (adminId === mainAdminId) {
-    res
-      .status(403)
-      .json({ error: "Főadmin felhasználó törlése nem lehetséges" });
-    return;
-  }
-
+  // Törlés az adatbázisból
   db.query("DELETE FROM admin_users WHERE id = ?", [adminId], (err, result) => {
+    // Hibakezelés
     if (err) {
-      console.error("Error deleting admin from the database:", err);
-      res.status(500).json({ error: "Database error" });
+      console.error("Hiba az admin felhasználó törlésekor:", err);
+      res.status(500).json({ error: "Adatbázis hiba" });
       return;
     }
 
     // Vizsgálat, hogy ténylegesen töröltünk-e elemet
     if (result.affectedRows === 0) {
-      res.status(404).json({ error: "Admin not found" });
-    } else {
-      res.json({ message: "Admin deleted successfully" });
+      res.status(404).json({ error: "Nincs ilyen admin felhasználó" });
+    }
+    // Sikeres eltávolítás
+    else {
+      res.json({ message: "Admin felhasználó sikeresen eltávolítva" });
     }
   });
 });
