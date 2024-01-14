16a94885250511478c1ad5e837a35081f20bcfad
Zsotya <mocsok2000@gmail.com>
2024-01-14 11:20:06 +0100

    Modified database connection to use pooling mechanism

--- backend/db.js
+++ backend/db.js
@@ -6,19 +6,22 @@ const connection = createConnection();
 
 // Kapcsolat létrehozása funkció
 function createConnection() {
-  return mysql.createConnection({
+  return mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 50,
+    queueLimit: 0,
   });
 }
 
 // Kapcsolódás kezelése, újrapróbálkozási logika
 function handleConnection() {
   // Kapcsolódási kísérlet
-  connection.connect((err) => {
+  connection.getConnection((err, conn) => {
     if (err) {
       console.error("Hiba az adatbázishoz kapcsolódáskor:", err);
       // Kapcsolódás újrapróbálása időzítés
@@ -26,6 +29,7 @@ function handleConnection() {
       setTimeout(handleConnection, 5000);
     } else {
       console.log("Sikeres kapcsolódás az adatbázishoz");
+      conn.release();
     }
   });
 }
